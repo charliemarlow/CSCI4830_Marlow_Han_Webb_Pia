b3ec624588b697a82fb6534ff89e5255a0cc4934
dhan1097 <dhh55489@uga.edu>
2019-05-06 16:30:13 -0400

    fixed mechanics for tutorial

--- VR_Final/Assets/Scripts/SetBoardTutorial.cs
+++ VR_Final/Assets/Scripts/SetBoardTutorial.cs
@@ -7,34 +7,43 @@ public class SetBoardTutorial : Tutorial
 
     public (int, int) lastHighlightLocation;
 
+
+
+    public override void pickupPiece(ChessPiece piece)
+    {
+        //Destroy(piece.gameObject);
+        Debug.Log("I picked up " + piece.name);
+    }
     public override void extraStart()
     {
         instantiateHighlight(0, 1);
+        kingTutorial(board.kingLightPrefab);
+        board.tutorial = this; //IMPORTANT
     }
 
     private void rookTutorial(GameObject rook)
     {
-        board.instantiatePiece(rook, 4, 3);
+        instantiatePiece(rook, 4, 3);
     }
 
     private void knightTutorial(GameObject knight)
     {
-        board.instantiatePiece(knight, 4, 3);
+        instantiatePiece(knight, 4, 3);
     }
 
     private void bishopTutorial(GameObject bishop)
     {
-        board.instantiatePiece(bishop, 4, 3);
+        instantiatePiece(bishop, 4, 3);
     }
 
     private void queenTutorial(GameObject queen)
     {
-        board.instantiatePiece(queen, 4, 3);
+        instantiatePiece(queen, 4, 3);
     }
 
     private void kingTutorial(GameObject king)
     {
-        board.instantiatePiece(king, 4, 3);
+        instantiatePiece(king, 4, 3);
     }
 
     private void setBoardTutorial(GameObject pawn, GameObject rook, GameObject knight, GameObject bishop, GameObject queen, GameObject king)
