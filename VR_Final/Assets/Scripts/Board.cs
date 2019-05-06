38091e605883b30c3b521156d00fea18f119f46d
charlie m <charmarlw@gmail.com>
2019-05-06 17:06:09 -0400

    Revert "small changes to the tutorial files"
    This reverts commit c36e64982b75561c48749c9940b6a5c38a02d225.

--- VR_Final/Assets/Scripts/Board.cs
+++ VR_Final/Assets/Scripts/Board.cs
@@ -645,7 +645,35 @@ public class Board : MonoBehaviour
             
         }
 
-    
+    private void setBoardPawn(GameObject pawn)
+    {
+        instantiatePiece(pawn, 4, 3);
+    }
+
+    private void setBoardRook(GameObject rook)
+    {
+        instantiatePiece(rook, 4, 3);
+    }
+
+    private void setBoardKnight(GameObject knight)
+    {
+        instantiatePiece(knight, 4, 3);
+    }
+
+    private void setBoardBishop(GameObject bishop)
+    {
+        instantiatePiece(bishop, 4, 3);
+    }
+
+    private void setBoardQueen(GameObject queen)
+    {
+        instantiatePiece(queen, 4, 3);
+    }
+
+    private void setBoardKing(GameObject king)
+    {
+        instantiatePiece(king, 4, 3);
+    }
 
     private void setBoardTutorial()
     {
--- VR_Final/Assets/Scripts/SetBoardTutorial.cs
+++ VR_Final/Assets/Scripts/SetBoardTutorial.cs
@@ -6,67 +6,41 @@ public class SetBoardTutorial : Tutorial
 {
 
     public (int, int) lastHighlightLocation;
-    public int i =0;
 
     public override void extraStart()
     {
         instantiateHighlight(0, 1);
-        placePiece(board.pawnLightPrefab);
     }
 
-
-    public override void pickupPiece(ChessPiece piece){
-
-    }
-
-    public override void dropPiece(ChessPiece piece){
-
-    }
-
-    public void setPawns(){
-
-    }
-
-    public void setRooks(){
-
-    }
-
-    public void setKnights(){
-
-    }
-
-    public void setBishops(){
-
-    }
-
-    public void highlightPlacement(){
-        if(i==1){
-            lastHighlightLocation=(0,1);
-            setPawns();
-        }
-        else if(i==2){
-            lastHighlightLocation=(0,0);
-            setRooks();
-        }
-        else if(i==3){
-            lastHighlightLocation=(0,1);
-            setKnights();
-        }
-        else if(i==4){
-            lastHighlightLocation=(0,2);
-            setBishops();
-        }
-        else if(i==5){
-            lastHighlightLocation=(0,3);
-        }
-        else if(i==6){
-            lastHighlightLocation=(0,4);
-        }
-    }
-
-    private void placePiece(GameObject piece)
+    private void rookTutorial(GameObject rook)
     {
-        board.instantiatePiece(piece, 4, 3);
+        board.instantiatePiece(rook, 4, 3);
+    }
+
+    private void knightTutorial(GameObject knight)
+    {
+        board.instantiatePiece(knight, 4, 3);
+    }
+
+    private void bishopTutorial(GameObject bishop)
+    {
+        board.instantiatePiece(bishop, 4, 3);
+    }
+
+    private void queenTutorial(GameObject queen)
+    {
+        board.instantiatePiece(queen, 4, 3);
+    }
+
+    private void kingTutorial(GameObject king)
+    {
+        board.instantiatePiece(king, 4, 3);
+    }
+
+    private void setBoardTutorial(GameObject pawn, GameObject rook, GameObject knight, GameObject bishop, GameObject queen, GameObject king)
+    {
+        //pawnTutorial(pawn);
+
     }
 
 
