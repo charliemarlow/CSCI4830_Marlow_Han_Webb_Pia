0024cbdfc8d4dad419b3ff8aeb620d4a59774b83
Charlie Marlow <cwm36477@uga.edu>
2019-05-06 17:09:08 -0400

    cleaned up script

--- VR_Final/Assets/Scripts/SetBoardTutorial.cs
+++ VR_Final/Assets/Scripts/SetBoardTutorial.cs
@@ -119,39 +119,6 @@ public class SetBoardTutorial : Tutorial
         index++;
     }
 
-    public void setBishops(){
-
-    }
-
-    public void highlightPlacement(){
-        if(i==1){
-            lastHighlightLocation=(0,1);
-            setPawns();
-        }
-        else if(i==2){
-            lastHighlightLocation=(0,0);
-            setRooks();
-        }
-        else if(i==3){
-            lastHighlightLocation=(0,1);
-            setKnights();
-        }
-        else if(i==4){
-            lastHighlightLocation=(0,2);
-            setBishops();
-        }
-        else if(i==5){
-            lastHighlightLocation=(0,3);
-        }
-        else if(i==6){
-            lastHighlightLocation=(0,4);
-        }
-    }
-
-    private void placePiece(GameObject piece)
-    {
-        board.instantiatePiece(piece, 4, 3);
-    }
 
 
 }
